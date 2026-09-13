199331d2dd5bb5db67cd62b9154f9cf982a0349d
tarangop302-max <tarangop302@gmail.com>
2026-09-13 14:17:06 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ const TOKEN           = T1 + T2;
 const CHANNEL_ID      = "1490713616813523004";
 const KING_CHANNEL_ID = "1515569728851017788";
 const ALERT_ROLE      = "<@&1493480046986268803>";
-const NTL_URL         = "https://ntl-slither.com/ss/rs.php";
+const NTL_URL         = "https://ntl-slither.com/ss/";
 const ALERT_INTERVAL  = 20000;
 
 let activePlayers    = new Set();
@@ -93,13 +93,18 @@ function fetchURL(url, cookie = "") {
   return new Promise((resolve, reject) => {
     const headers = {
       "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36",
-      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
-      "Accept-Language": "en-US,en;q=0.9",
+      "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+      "Accept-Language": "en-US,en;q=0.9,en-IN;q=0.8",
       "Accept-Encoding": "identity",
       "Cache-Control": "no-cache",
       "Pragma": "no-cache",
-      "Referer": "https://ntl-slither.com/",
+      "Referer": "https://ntl-slither.com/ss/",
+      "Origin": "https://ntl-slither.com",
       "Connection": "keep-alive",
+      "Upgrade-Insecure-Requests": "1",
+      "Sec-Fetch-Dest": "document",
+      "Sec-Fetch-Mode": "navigate",
+      "Sec-Fetch-Site": "same-origin",
     };
     if (cookie) headers["Cookie"] = cookie;
 
